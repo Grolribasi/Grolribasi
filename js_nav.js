5bdadaee9be31c89644b85eea5d31d141c7f7b43
Grolribasi <grolribasi@gmail.com>
2021-04-16 21:31:51 +0300

    new menu and breadcrumbs

--- js_nav.js
+++ js_nav.js
@@ -1,13 +1,3 @@
-function myFunction() {
-    var x = document.getElementById("topLinks");
-    if (x.style.display === "block") {
-            x.style.display = "none";
-    } 
-    else {
-    x.style.display = "block";
-    }
-}
-
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
 }
