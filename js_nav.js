7e73147e372c7073f55da1b06a7335ae815d500c
Vladimir <markiev.v@docsvision.com>
2021-04-17 13:56:41 +0300

    Menu now hides away

--- js_nav.js
+++ js_nav.js
@@ -1,8 +1,19 @@
 function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
+	document.getElementById("mySidebar").style.width = "250px";
 }
 
 function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
+ 	document.getElementById("mySidebar").style.width = "0";
 
+}
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("breadcrumb").style.top = "0";
+  } else {
+    document.getElementById("breadcrumb").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
 }
--- js_stub.js
+++ js_stub.js
@@ -1,6 +1,6 @@
-            var prevScrollPos = window.pageYOffset;
-            window.onscroll = function() {
-                var currentScrollPos = window.pageYOffset;
-                document.getElementById("stub").style.display = prevScrollPos > currentScrollPos ? "block" : "none";
-                prevScrollPos = currentScrollPos;
-            }
+var prevScrollPos = window.pageYOffset;
+	window.onscroll = function() {
+	var currentScrollPos = window.pageYOffset;
+	document.getElementById("stub").style.display = prevScrollPos > currentScrollPos ? "block" : "none";
+	prevScrollPos = currentScrollPos;
+}
