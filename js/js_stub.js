a7318b22a0a82bb198a7a3e94be5d8958dca4692
Grolribasi <grolribasi@gmail.com>
2021-04-24 21:12:01 +0300

    Улучшения. Переписал текст на страницах навыков и опыта, начал переписывать страницу о себе, немного изменил код и цвета.

--- js/js_stub.js
+++
@@ -1,6 +0,0 @@
-var prevScrollPos = window.pageYOffset;
-	window.onscroll = function() {
-	var currentScrollPos = window.pageYOffset;
-	document.getElementById("stub").style.display = prevScrollPos > currentScrollPos ? "block" : "none";
-	prevScrollPos = currentScrollPos;
-}
